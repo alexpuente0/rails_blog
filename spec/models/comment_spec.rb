b69eaeace0738f3a96e68406c0d730b3b75f2712
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-24 10:32:12 -0500

    Refactor testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,35 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  this_user = User.new(name: 'Lou', photo: 'photo_URL', bio: 'Test Dummy', post_counter: 0)
-  this_post = Post.new(author: this_user, title: 'new post', text: 'Lorem Ipsum etc.', comments_counter: 0,
-                       likes_counter: 0)
-  comment = Comment.new(author: this_user, post: this_post, text: 'New Comment Who Dis')
+  subject(:this_user) { User.create(name: 'Lou', photo: 'photo_URL', bio: 'Test Dummy', post_counter: 0) }
+  subject(:this_post) do
+    Post.create(author: this_user, title: 'new post', text: 'Lorem Ipsum etc.', comments_counter: 0,
+                likes_counter: 0)
+  end
+  subject(:comment) { Comment.create(author: this_user, post: this_post, text: 'New Comment Who Dis') }
+
+  after(:all) do
+    Comment.destroy_all
+    Post.destroy_all
+    User.destroy_all
+  end
+
+  it 'creates a comment when valid' do
+    expect(comment).to be_valid
+  end
+
+  it 'Saves comment and loads it from database' do
+    expect(comment).to be_an_instance_of(Comment)
+  end
 
   before do
-    this_user.save
-    this_post.save
-    comment.save
+    this_post.comments.create(author: this_user, post: this_post, text: 'New comment #1')
+    this_post.comments.create(author: this_user, post: this_post, text: 'New comment #2')
   end
-
-  context 'Comment Validation' do
-    it 'creates a comment when valid' do
-      expect(comment).to be_valid
-    end
-  end
-
-  context 'Save and Load Comment' do
-    it 'Saves comment and loads it from database' do
-      expect(comment).to be_an_instance_of(Comment)
-    end
-  end
-
-  context 'Update comment counter' do
-    before do
-      Comment.new(author: this_user, post: this_post, text: 'Another Comment')
-    end
-    it 'should add +1 to comment counter' do
-      expect(this_post.comments_counter).to eq(3)
-    end
+  it 'should add +1 to comment counter' do
+    expect(this_post.comments_counter).to eq(2)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,58 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  this_user = User.new(name: 'Luke', photo: 'photo_URL', bio: 'Test Dummy', post_counter: 1)
-  this_user.save
+  before do
+    @this_user = User.new(name: 'Luke', photo: 'photo_URL', bio: 'Test Dummy', post_counter: 1)
+    @this_post = Post.new(author: @this_user, title: 'Appropiate Title', text: 'Lorem Ipsum etc', comments_counter: 1,
+                          likes_counter: 1)
+    @this_post.save
+  end
 
-  this_post = Post.new(author: this_user, title: 'Appropiate Title', text: 'Lorem Ipsum etc', comments_counter: 1,
-                       likes_counter: 1)
-  this_post.save
+  after(:all) do
+    Like.destroy_all
+    Comment.destroy_all
+    Post.destroy_all
+    User.destroy_all
+  end
 
-  describe 'Post Validation' do
-    context 'Successful Case'
-    it 'Validation is Succesful new Post created' do
-      expect(this_post).to be_valid
+  it 'Validation is Succesful new Post created' do
+    @this_post.likes_counter = 0
+    @this_post.comments_counter = 0
+    expect(@this_post).to be_valid
+  end
+
+  it 'Post Validation Fails for Title over 250 chars' do
+    @this_post.likes_counter = 0
+    @this_post.comments_counter = 0
+    @this_post.title = 'o' * 255
+    expect(@this_post).not_to be_valid
+  end
+
+  context 'Fails' do
+    before { @this_post.title = nil }
+    it 'if no title' do
+      expect(@this_post).not_to be_valid
     end
   end
 
-  describe 'Post Validation Fails for Title' do
-    context 'Fails' do
-      before { this_post.title = 'o' * 255 }
-      it 'if title over 250 chars' do
-        expect(this_post).not_to be_valid
-      end
-    end
-
-    context 'Fails' do
-      before { this_post.title = nil }
-      it 'if no title' do
-        expect(this_post).not_to be_valid
-      end
-    end
-
-    context 'Fails' do
-      before { this_post.title = '' }
-      it 'if blank title on post' do
-        expect(this_post).not_to be_valid
-      end
+  context 'Fails' do
+    before { @this_post.title = '' }
+    it 'if blank title on post' do
+      expect(@this_post).not_to be_valid
     end
   end
 
   describe 'Post Validation Fails for Comment Counter' do
     context 'Fails' do
       before do
-        this_post.title = 'Appropiate Title'
-        this_post.comments_counter = -5
+        @this_post.title = 'Appropiate Title'
+        @this_post.comments_counter = -5
       end
       it 'if negative comment count' do
-        expect(this_post).not_to be_valid
+        expect(@this_post).not_to be_valid
       end
     end
 
     context 'Fails' do
-      before { this_post.comments_counter = 'eight' }
+      before { @this_post.comments_counter = 'eight' }
       it 'if string comment count' do
-        expect(this_post).not_to be_valid
+        expect(@this_post).not_to be_valid
       end
     end
   end
@@ -60,38 +64,46 @@ RSpec.describe Post, type: :model do
   describe 'Post Validation Fails for Likes Counter' do
     context 'Fails' do
       before do
-        this_post.comments_counter = 1
-        this_post.likes_counter = -5
+        @this_post.comments_counter = 1
+        @this_post.likes_counter = -5
       end
       it 'if negative like count' do
-        expect(this_post).not_to be_valid
+        expect(@this_post).not_to be_valid
       end
     end
 
     context 'Fails' do
-      before { this_post.likes_counter = 'eight' }
+      before { @this_post.likes_counter = 'eight' }
       it 'if string like count' do
-        expect(this_post).not_to be_valid
+        expect(@this_post).not_to be_valid
       end
     end
   end
 
-  describe 'Method Functionality' do
-    context 'Recent comment method works properly' do
-      it 'returns a valid type' do
-        expect(this_post.recent_comments.length).to eq(0)
-        Comment.create(author: this_user, post: this_post, text: 'FIRST!')
-        expect(this_post.recent_comments.length).to eq(1)
-      end
-    end
+  it 'Method Functionality for Recent posts' do
+    @this_post.likes_counter = 0
+    @this_post.comments_counter = 0
+    @this_post.save
+    Comment.create(author: @this_user, post: @this_post, text: 'Text1')
+    comment2 = Comment.create(author: @this_user, post: @this_post, text: 'Text2')
+    comment3 = Comment.create(author: @this_user, post: @this_post, text: 'Text3')
+    comment4 = Comment.create(author: @this_user, post: @this_post, text: 'Text4')
+    comment5 = Comment.create(author: @this_user, post: @this_post, text: 'Text5')
+    comment6 = Comment.create(author: @this_user, post: @this_post, text: 'Text6')
+    comment7 = Comment.create(author: @this_user, post: @this_post, text: 'Text7')
+    comment8 = Comment.create(author: @this_user, post: @this_post, text: 'Text8')
+    comment9 = Comment.create(author: @this_user, post: @this_post, text: 'Text9')
+    comment10 = Comment.create(author: @this_user, post: @this_post, text: 'Text10')
+    expect(@this_post.recent_comments.length).to eq(5)
+  end
 
-    context 'Update posts counter' do
-      before do
-        Post.new(author: this_user, title: 'new post', text: 'Lorem Ipsum etc', comments_counter: 0, likes_counter: 0)
-      end
-      it 'should add +1 to posts counter' do
-        expect(this_user.post_counter).to eq(2)
-      end
+  context 'Update posts counter' do
+    before do
+      Post.new(author: @this_user, title: 'new post', text: 'Lorem Ipsum etc', comments_counter: 0,
+               likes_counter: 0)
+    end
+    it 'should add +1 to posts counter' do
+      expect(@this_user.post_counter).to eq(2)
     end
   end
 end
