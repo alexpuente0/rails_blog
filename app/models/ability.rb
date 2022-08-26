aa13ab4b03af49b99b2f576afde6efda3a5718c6
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-26 17:11:06 -0500

    Add permission to user to delete own and admin to delete all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,7 @@ class Ability
     return unless user&.role == 'admin'
 
     can :manage, :all
+    
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
-  has_many :likes, class_name: 'Likes', foreign_key: 'author_id'
-  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id', dependent: :delete_all
+  has_many :likes, class_name: 'Likes', foreign_key: 'author_id', dependent: :delete_all
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id', dependent: :delete_all
 
   validates :name, presence: true
   validates :post_counter, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
